6f1a0d36249f3cfc3cdea3f40258a1ec5b03865b
ljlopez <ljlopez@fundacionsafa.es>
2022-04-07 19:06:45 +0200

    subida inicial

--- libreria/urls.py
+++ libreria/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('inicio/', cargar_inicio),
     path('libros/', cargar_vista_libros),
     path('libros/crear/', crear_libro, name='crear_libro'),
-    path('libros/<int:id>',detalle_libro, name='detalle_libro')
+    path('libros/<int:id>',detalle_libro, name='detalle_libro'),
+    path('libros/editar/<int:id>', editar_libro, name='editar_libro'),
+    path('libros/borrar/<int:id>', borrar_libro, name='borrar_libro')
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -29,10 +29,33 @@ def crear_libro(request):
         return render(request, 'crear_libro.html')
 
 
+def editar_libro(request,id):
+    if request.method == "GET":
+        libro = Libro.objects.get(id=id)
+        return render(request, 'editar_libro.html', {'libro': libro})
+    else:
+        libro = Libro()
+        libro.id = id
+        libro.nombre = request.POST.get('nombre')
+        libro.num_paginas = request.POST.get('num_paginas')
+        libro.fecha_publicacion = request.POST.get('fecha')
+        libro.editorial = Editorial.objects.get(id=1)
+        Libro.save(libro)
+        return redirect('/libreria/libros')
+
+
+
 def detalle_libro(request, id):
     libro = Libro.objects.get(id=id)
     context = {'libro': libro}
     return render(request, 'libro_detalle.html', context)
+
+
+def borrar_libro(request,id):
+    libro = Libro.objects.get(id=id)
+    if libro is not None:
+        Libro.delete(libro)
+    return redirect('/libreria/libros')
 
 
 
